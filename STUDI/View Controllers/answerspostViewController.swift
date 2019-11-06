a80f602d49d0d6f5fd978164f06e0c1996003171
Amos <53104046+WiseAmos@users.noreply.github.com>
2019-11-06 12:13:00 +0800

    adasdasda

---
+++ STUDI/View Controllers/answerspostViewController.swift
@@ -0,0 +1,31 @@
+//
+//  answerspostViewController.swift
+//  STUDI
+//
+//  Created by Amos Goh En Jie on 6/11/19.
+//  Copyright © 2019 Mohamed Niyaz. All rights reserved.
+//
+
+import UIKit
+
+class answerspostViewController: UIViewController {
+
+    @IBOutlet weak var Answers: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+
+}
